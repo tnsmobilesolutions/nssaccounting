ba27e3f3f0c8b972d22889f5bab3b6310a7cd941
Manas137 <iamsagar.137@gmail.com>
2021-11-08 23:59:42 +0530

    payment model added

--- lib/model/payment.dart
+++ lib/model/payment.dart
@@ -1,52 +1,89 @@
 import 'dart:convert';
 
 class Payment {
-  final String? accountCode;
-  final String? amount;
-  final String? paymentDate;
-  final String? paymentId;
   final String? voucherNo;
+  final String? paymentDate;
+  final String? accountHead;
+  final String? partyName;
+  final String? amount;
+  final String? paymentType;
+  final String? paymentMode;
+  final String? transactinRefNo;
+  final String? preparedBy;
+  final String? authorizedBy;
+  final String? remark;
   Payment({
-    this.accountCode,
-    this.amount,
-    this.paymentDate,
-    this.paymentId,
     this.voucherNo,
+    this.paymentDate,
+    this.accountHead,
+    this.partyName,
+    this.amount,
+    this.paymentType,
+    this.paymentMode,
+    this.transactinRefNo,
+    this.preparedBy,
+    this.authorizedBy,
+    this.remark,
   });
 
   Payment copyWith({
-    String? accountCode,
-    String? amount,
-    String? paymentDate,
-    String? paymentId,
     String? voucherNo,
+    String? paymentDate,
+    String? accountHead,
+    String? partyName,
+    String? amount,
+    String? paymentType,
+    String? paymentMode,
+    String? transactinRefNo,
+    String? preparedBy,
+    String? authorizedBy,
+    String? remark,
   }) {
     return Payment(
-      accountCode: accountCode ?? this.accountCode,
-      amount: amount ?? this.amount,
-      paymentDate: paymentDate ?? this.paymentDate,
-      paymentId: paymentId ?? this.paymentId,
       voucherNo: voucherNo ?? this.voucherNo,
+      paymentDate: paymentDate ?? this.paymentDate,
+      accountHead: accountHead ?? this.accountHead,
+      partyName: partyName ?? this.partyName,
+      amount: amount ?? this.amount,
+      paymentType: paymentType ?? this.paymentType,
+      paymentMode: paymentMode ?? this.paymentMode,
+      transactinRefNo: transactinRefNo ?? this.transactinRefNo,
+      preparedBy: preparedBy ?? this.preparedBy,
+      authorizedBy: authorizedBy ?? this.authorizedBy,
+      remark: remark ?? this.remark,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'accountCode': accountCode,
-      'amount': amount,
-      'paymentDate': paymentDate,
-      'paymentId': paymentId,
       'voucherNo': voucherNo,
+      'paymentDate': paymentDate,
+      'accountHead': accountHead,
+      'partyName': partyName,
+      'amount': amount,
+      'paymentType': paymentType,
+      'paymentMode': paymentMode,
+      'transactinRefNo': transactinRefNo,
+      'preparedBy': preparedBy,
+      'authorizedBy': authorizedBy,
+      'remark': remark,
     };
   }
 
   factory Payment.fromMap(Map<String, dynamic> map) {
     return Payment(
-      accountCode: map['accountCode'] != null ? map['accountCode'] : null,
-      amount: map['amount'] != null ? map['amount'] : null,
-      paymentDate: map['paymentDate'] != null ? map['paymentDate'] : null,
-      paymentId: map['paymentId'] != null ? map['paymentId'] : null,
       voucherNo: map['voucherNo'] != null ? map['voucherNo'] : null,
+      paymentDate: map['paymentDate'] != null ? map['paymentDate'] : null,
+      accountHead: map['accountHead'] != null ? map['accountHead'] : null,
+      partyName: map['partyName'] != null ? map['partyName'] : null,
+      amount: map['amount'] != null ? map['amount'] : null,
+      paymentType: map['paymentType'] != null ? map['paymentType'] : null,
+      paymentMode: map['paymentMode'] != null ? map['paymentMode'] : null,
+      transactinRefNo:
+          map['transactinRefNo'] != null ? map['transactinRefNo'] : null,
+      preparedBy: map['preparedBy'] != null ? map['preparedBy'] : null,
+      authorizedBy: map['authorizedBy'] != null ? map['authorizedBy'] : null,
+      remark: map['remark'] != null ? map['remark'] : null,
     );
   }
 
@@ -57,7 +94,7 @@ class Payment {
 
   @override
   String toString() {
-    return 'Payment(accountCode: $accountCode, amount: $amount, paymentDate: $paymentDate, paymentId: $paymentId, voucherNo: $voucherNo)';
+    return 'Payment(voucherNo: $voucherNo, paymentDate: $paymentDate, accountHead: $accountHead, partyName: $partyName, amount: $amount, paymentType: $paymentType, paymentMode: $paymentMode, transactinRefNo: $transactinRefNo, preparedBy: $preparedBy, authorizedBy: $authorizedBy, remark: $remark)';
   }
 
   @override
@@ -65,19 +102,31 @@ class Payment {
     if (identical(this, other)) return true;
 
     return other is Payment &&
-        other.accountCode == accountCode &&
-        other.amount == amount &&
+        other.voucherNo == voucherNo &&
         other.paymentDate == paymentDate &&
-        other.paymentId == paymentId &&
-        other.voucherNo == voucherNo;
+        other.accountHead == accountHead &&
+        other.partyName == partyName &&
+        other.amount == amount &&
+        other.paymentType == paymentType &&
+        other.paymentMode == paymentMode &&
+        other.transactinRefNo == transactinRefNo &&
+        other.preparedBy == preparedBy &&
+        other.authorizedBy == authorizedBy &&
+        other.remark == remark;
   }
 
   @override
   int get hashCode {
-    return accountCode.hashCode ^
-        amount.hashCode ^
+    return voucherNo.hashCode ^
         paymentDate.hashCode ^
-        paymentId.hashCode ^
-        voucherNo.hashCode;
+        accountHead.hashCode ^
+        partyName.hashCode ^
+        amount.hashCode ^
+        paymentType.hashCode ^
+        paymentMode.hashCode ^
+        transactinRefNo.hashCode ^
+        preparedBy.hashCode ^
+        authorizedBy.hashCode ^
+        remark.hashCode;
   }
 }
