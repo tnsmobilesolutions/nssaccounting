fd53232ff1fc281321b9efcd9915f95a25baab97
Atanu <jena.atanusabyasachi@gmail.com>
2022-01-14 11:21:21 +0530

    search bar incrimented

--- lib/data/branchAPI.dart
+++ lib/data/branchAPI.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:nssaccounting/main.dart';
 import 'package:nssaccounting/model/branch.dart';
+//import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+//import 'package:flutter/material.dart';
 
 class BranchAPI {
   Future<List<Branch?>> getAllBranches() {
@@ -32,6 +35,13 @@ class BranchAPI {
       return lstBranch;
     });
     return lstBranches;
+
+    // Future queryData(String queryString) async {
+    //   return FirebaseFirestore.instance
+    //       .collection('branches')
+    //       .where('branchName', isGreaterThanOrEqualTo: queryString)
+    //       .get();
+    // }
   }
 
   Future<String> createNewBranch(Branch branch) async {
@@ -74,3 +84,271 @@ class BranchAPI {
         .update(branch.toMap());
   }
 }
+
+
+/*
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Search Bar App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const historyLength = 5;
+
+  List<String> _searchHistory = [
+    'fuchsia',
+    'flutter',
+    'widgets',
+    'resocoder',
+    // 'Anjaw',
+    // 'Anuppur',
+    // 'Araria',
+    // 'Ariyalur',
+    // 'Arwal',
+    // 'Ashok Nagar',
+    // 'Auraiya',
+    // 'Aurangabad',
+    // 'Aurangabad',
+    // 'Azamgarh',
+    // 'Wayanad',
+    // 'West Champaran',
+    // 'West Delhi',
+    // 'West Garo Hills',
+    // 'West Kameng',
+    // 'West Khasi Hills',
+    // 'West Siang',
+    // 'West Sikkim',
+    // 'West Singhbhum',
+    // 'West Tripura',
+    // 'Wokha',
+    // 'Yadgir',
+    // 'Yamuna Nagar',
+    // 'Yanam',
+    // 'Yavatmal',
+    // 'Zunheboto',
+  ];
+
+  List<String> filteredSearchHistory;
+
+  String selectedTerm;
+
+  //this method filters the search like h, he, hey
+  List<String> filterSearchTerms({
+    @required String filter,
+  }) {
+    if (filter != null && filter.isNotEmpty) {
+      return _searchHistory.reversed
+          .where((term) => term.startsWith(filter))
+          .toList();
+    } else {
+      return _searchHistory.reversed.toList();
+    }
+  }
+
+  //adds the recent search term
+  void addSearchTerm(String term) {
+    if (_searchHistory.contains(term)) {
+      putSearchTermFirst(term);
+      return;
+    }
+    _searchHistory.add(term);
+    if (_searchHistory.length > historyLength) {
+      _searchHistory.removeRange(0, _searchHistory.length - historyLength);
+    }
+
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  // deletes the search history when someone touches 'x'
+  void deleteSearchTerm(String term) {
+    _searchHistory.removeWhere((t) => t == term);
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  // puts recent search item at top
+  void putSearchTermFirst(String term) {
+    deleteSearchTerm(term);
+    addSearchTerm(term);
+  }
+
+  FloatingSearchBarController controller;
+
+  // initiates the search
+  @override
+  void initState() {
+    super.initState();
+    controller = FloatingSearchBarController();
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FloatingSearchBar(
+        controller: controller,
+        body: FloatingSearchBarScrollNotifier(
+          child: SearchResultsListView(
+            searchTerm: selectedTerm,
+          ),
+        ),
+        transition: CircularFloatingSearchBarTransition(),
+        physics: BouncingScrollPhysics(),
+        title: Text(
+          selectedTerm ?? 'The Search App',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        hint: 'Search branch',
+        actions: [
+          FloatingSearchBarAction.searchToClear(),
+        ],
+
+        //when searched item is started being searched
+        onQueryChanged: (query) {
+          setState(() {
+            filteredSearchHistory = filterSearchTerms(filter: query);
+          });
+        },
+        onSubmitted: (query) {
+          setState(() {
+            addSearchTerm(query);
+            selectedTerm = query;
+          });
+          controller.close();
+        },
+        builder: (context, transition) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(6), // earch history body
+            child: Material(
+              color: Colors.yellow[100],
+              elevation: 4,
+              child: Builder(
+                builder: (context) {
+                  // if (filteredSearchHistory.isEmpty &&
+                  //     controller.query.isEmpty) {
+                  //   return Container(
+                  //     height: 56,
+                  //     width: double.infinity,
+                  //     alignment: Alignment.center,
+                  //     // child: Text(
+                  //     //   'Start searching',
+                  //     //   maxLines: 10,
+                  //     //   overflow: TextOverflow.ellipsis,
+                  //     //   style: Theme.of(context).textTheme.caption,
+                  //     // ),
+                  //   );
+                  // } else
+                  if (filteredSearchHistory.isEmpty) {
+                    return ListTile(
+                      title: Text(controller.query),
+                      leading: const Icon(Icons.search),
+                      onTap: () {
+                        setState(() {
+                          addSearchTerm(controller.query);
+                          selectedTerm = controller.query;
+                        });
+                        controller.close();
+                      },
+                    );
+                  } else {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: filteredSearchHistory
+                          .map(
+                            (term) => ListTile(
+                              title: Text(
+                                term,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              leading: const Icon(Icons.history),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.clear),
+                                onPressed: () {
+                                  setState(() {
+                                    deleteSearchTerm(term);
+                                  });
+                                },
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  putSearchTermFirst(term);
+                                  selectedTerm = term;
+                                });
+                                controller.close();
+                              },
+                            ),
+                          )
+                          .toList(),
+                    );
+                  }
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SearchResultsListView extends StatelessWidget {
+  final String searchTerm;
+
+  const SearchResultsListView({
+    Key key,
+    @required this.searchTerm,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (searchTerm == null) {
+      return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.search,
+              size: 64,
+            ),
+            Text(
+              'Start searching',
+              style: Theme.of(context).textTheme.headline5,
+            )
+          ],
+        ),
+      );
+    }
+
+    //final fsb = FloatingSearchBar.of(context);
+
+    return ListView(
+      //padding: EdgeInsets.only(top: fsb.),//top: fsb.height + fsb.margins.vertical),
+      children: List.generate(
+        40,
+        (index) => ListTile(
+          title: Text('$searchTerm'),
+          subtitle: Text(index.toString()),
+        ),
+      ),
+    );
+  }
+}
+*/
--- lib/setting_icon_pages/manage_branch.dart
+++ lib/setting_icon_pages/manage_branch.dart
@@ -1,10 +1,12 @@
-import 'dart:ui';
+//import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:nssaccounting/common_widgets/common_style.dart';
 import 'package:nssaccounting/model/branch.dart';
 import 'package:nssaccounting/setting_icon_pages/add_manage_branch.dart';
 import 'package:nssaccounting/setting_icon_pages/edit_manage_branch.dart';
+import 'package:nssaccounting/data/branchAPI.dart';
 
 class ManageBranch extends StatefulWidget {
   ManageBranch({Key? key}) : super(key: key);
@@ -16,135 +18,211 @@ class ManageBranch extends StatefulWidget {
 class _ManageBranchState extends State<ManageBranch> {
   Branch? _branch;
 
+  static const historyLength = 5;
+
+  List<String> _searchHistory = [
+    'atanu',
+    'amrut',
+    'abhishek',
+    'aabha',
+    'arnav',
+    'badrinath',
+    'binayak',
+    'biswaprakash',
+    'biswanath',
+    'chetan',
+    'chintu',
+    'dinesh',
+    'digambar',
+  ];
+
+  List<String>? filteredSearchHistory;
+
+  String? selectedTerm;
+
+  //this method filters the search like h, he, hey
+  List<String> filterSearchTerms({
+    @required String? filter,
+  }) {
+    if (filter != null && filter.isNotEmpty) {
+      final branches = BranchAPI().getBranchByName(filter);
+      return _searchHistory
+          .where((branch) => branch.startsWith(filter))
+          .toList();
+    } else {
+      return _searchHistory.toList();
+    }
+  }
+
+  //adds the recent search branch
+  void addSearchTerm(String branch) {
+    if (_searchHistory.contains(branch)) {
+      putSearchTermFirst(branch);
+      return;
+    }
+    _searchHistory.add(branch);
+    if (_searchHistory.length > historyLength) {
+      _searchHistory.removeRange(0, _searchHistory.length - historyLength);
+    }
+
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  // deletes the search history when someone touches 'x'
+  void deleteSearchTerm(String branch) {
+    _searchHistory.removeWhere((t) => t == branch);
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  // puts recent search item at top
+  void putSearchTermFirst(String branch) {
+    deleteSearchTerm(branch);
+    addSearchTerm(branch);
+  }
+
+  late FloatingSearchBarController controller;
+
+  // initiates the search
+  @override
+  void initState() {
+    super.initState();
+    controller = FloatingSearchBarController();
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Manage Branch'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'search', icon: Icon(Icons.search)),
+      body: FloatingSearchBar(
+        automaticallyImplyBackButton: false,
+        closeOnBackdropTap: false,
+        controller: controller,
+        // body: FloatingSearchBarScrollNotifier(
+        //   child: SearchResultsListView(
+        //     searchTerm: selectedTerm,
+        //   ),
+        // ),
+        transition: CircularFloatingSearchBarTransition(),
+        physics: BouncingScrollPhysics(),
+        title: Text(
+          selectedTerm ?? 'Search',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        hint: 'Search branch',
+        actions: [
+          FloatingSearchBarAction.searchToClear(),
+        ],
+
+        //when searched item is started being searched
+        onQueryChanged: (query) async {
+          print(query);
+          setState(() {
+            filteredSearchHistory = filterSearchTerms(filter: query);
+          });
+        },
+        onSubmitted: (query) {
+          setState(() {
+            addSearchTerm(query);
+            selectedTerm = query;
+          });
+          controller.close();
+        },
+        builder: (context, transition) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(6), // search history body
+            child: Material(
+              color: Colors.yellow[100],
+              elevation: 4,
+              child: Builder(
+                builder: (context) {
+                  if (filteredSearchHistory!.isEmpty &&
+                      controller.query.isEmpty) {
+                    return Container(
+                      height: 56,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Start searching',
+                        maxLines: 10,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.caption,
+                      ),
+                    );
+                  } else if (filteredSearchHistory!.isEmpty) {
+                    return ListTile(
+                      title: Text(controller.query),
+                      leading: const Icon(Icons.search),
+                      onTap: () {
+                        setState(() {
+                          addSearchTerm(controller.query);
+                          selectedTerm = controller.query;
+                        });
+                        controller.close();
+                      },
+                    );
+                  } else {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: filteredSearchHistory!
+                          .map(
+                            (branch) => ListTile(
+                              title: Text(
+                                branch,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              leading: const Icon(Icons.history),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.clear),
+                                onPressed: () {
+                                  setState(() {
+                                    deleteSearchTerm(branch);
+                                  });
+                                },
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  putSearchTermFirst(branch);
+                                  selectedTerm = branch;
+                                });
+                                controller.close();
+                              },
+                            ),
+                          )
+                          .toList(),
+                    );
+                  }
+                },
               ),
-              SizedBox(height: 24),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.lightBlue[100]),
-                padding: EdgeInsets.fromLTRB(18, 5, 18, 5),
-                child: Text(
-                  'Name : Mumbai',
-                  style: TextStyle(fontSize: 22),
-                ),
-                width: double.infinity,
-              ),
-              SizedBox(height: 12),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.lightBlue[100]),
-                padding: EdgeInsets.fromLTRB(18, 10, 18, 10),
-                child: Text(
-                  'Address : 500,Taloja',
-                  style: TextStyle(fontSize: 22),
-                ),
-                width: double.infinity,
-              ),
-              SizedBox(height: 12),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.lightBlue[100]),
-                padding: EdgeInsets.fromLTRB(18, 10, 18, 10),
-                child: Text(
-                  'City : Mumbai',
-                  style: TextStyle(fontSize: 22),
-                ),
-                width: double.infinity,
-              ),
-              SizedBox(height: 12),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.lightBlue[100]),
-                padding: EdgeInsets.fromLTRB(18, 10, 18, 10),
-                child: Text(
-                  'State : MH',
-                  style: TextStyle(fontSize: 22),
-                ),
-                width: double.infinity,
-              ),
-              SizedBox(height: 12),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.lightBlue[100]),
-                padding: EdgeInsets.fromLTRB(18, 10, 18, 10),
-                child: Text(
-                  'Country : India',
-                  style: TextStyle(fontSize: 22),
-                ),
-                width: double.infinity,
-              ),
-              SizedBox(height: 12),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.lightBlue[100]),
-                padding: EdgeInsets.fromLTRB(18, 10, 18, 10),
-                child: Text(
-                  'Pin : 40210',
-                  style: TextStyle(fontSize: 22),
-                ),
-                width: double.infinity,
-              ),
-              SizedBox(height: 12),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.lightBlue[100]),
-                padding: EdgeInsets.fromLTRB(18, 10, 18, 10),
-                child: Text(
-                  'Numbers of Devotees : 83',
-                  style: TextStyle(fontSize: 22),
-                ),
-                width: double.infinity,
-              ),
-              SizedBox(height: 12),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.lightBlue[100]),
-                padding: EdgeInsets.fromLTRB(18, 10, 18, 10),
-                child: Text(
-                  'Est. Year : 2012 ',
-                  style: TextStyle(fontSize: 22),
-                ),
-                width: double.infinity,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                  style: CommonStyle.elevatedSubmitButtonStyle(),
-                  onPressed: () {
-                    if (_branch != null) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ManageEditPage(
-                                    branch: _branch!,
-                                  )));
-                    }
-                  },
-                  child: Text(
-                    'Edit',
-                    style: TextStyle(fontSize: 24),
-                  ))
-            ],
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: ElevatedButton(
+          style: CommonStyle.elevatedSubmitButtonStyle(),
+          onPressed: () {
+            if (_branch != null) {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ManageEditPage(
+                            branch: _branch!,
+                          )));
+            }
+          },
+          child: Text(
+            'Edit',
+            style: TextStyle(fontSize: 24),
           ),
         ),
       ),
@@ -154,6 +232,44 @@ class _ManageBranchState extends State<ManageBranch> {
               MaterialPageRoute(builder: (context) => ManageAddBranch()));
         },
         child: Text('Add'),
+      ),
+    );
+  }
+}
+
+class SearchResultsListView extends StatelessWidget {
+  late final String searchTerm;
+
+  @override
+  Widget build(BuildContext context) {
+    if (searchTerm == null) {
+      return Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.search,
+              size: 64,
+            ),
+            Text(
+              'Start searching',
+              style: Theme.of(context).textTheme.headline5,
+            )
+          ],
+        ),
+      );
+    }
+
+    //final fsb = FloatingSearchBar.of(context);
+
+    return ListView(
+      //padding: EdgeInsets.only(top: fsb.),//top: fsb.height + fsb.margins.vertical),
+      children: List.generate(
+        40,
+        (index) => ListTile(
+          title: Text('$searchTerm Start searching'),
+          subtitle: Text(index.toString()),
+        ),
       ),
     );
   }
