4f9761a5feb0aefd2c98b9e55e87f69f4085e831
Manas137 <iamsagar.137@gmail.com>
2022-02-01 08:08:17 +0530

    payment voucher

--- lib/dashboard_pages/payment_voucher.dart
+++ lib/dashboard_pages/payment_voucher.dart
@@ -1,20 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:nssaccounting/common_widgets/payment_widget.dart';
+import 'package:nssaccounting/search/receipt_preview.dart';
+import 'package:nssaccounting/utility.dart';
+import 'package:nssaccounting/common_widgets/common_style.dart';
+import 'package:nssaccounting/data/auth.dart';
+import 'package:nssaccounting/data/receiptAPI.dart';
+import 'package:nssaccounting/model/receipt.dart';
 
 class PaymentVoucher extends StatefulWidget {
-  PaymentVoucher({Key? key}) : super(key: key);
-
+  const PaymentVoucher({Key? key}) : super(key: key);
   @override
   State<PaymentVoucher> createState() => _PaymentVoucherState();
 }
 
 class _PaymentVoucherState extends State<PaymentVoucher> {
+  bool notMember = false;
+  DateTime? _dateTime;
+
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameController = TextEditingController();
+  final _amountController = TextEditingController();
+  final _transactionController = TextEditingController();
+  final _approverNameController = TextEditingController();
+
+  final _paymentInfo = PaymentInfo();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Payment Voucher"),
       ),
-      body: Text('Payment Voucher content goes here'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 10),
+                TextFormField(
+                  keyboardType: TextInputType.name,
+                  controller: _nameController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please Enter Your Name';
+                    } else if (!RegExp(
+                            r'^[a-zA-Z0-9]+(?:[\w -]*[a-zA-Z0-9]+)*$')
+                        .hasMatch(value)) {
+                      return 'Please Enter Correct Name';
+                    }
+                    return null;
+                  },
+                  // style: TextStyle(height: 0.5),
+                  decoration: CommonStyle.textFieldStyle(
+                    labelTextStr: "Enter Name",
+                    hintTextStr: "Enter Name",
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: _amountController,
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$')
+                            .hasMatch(value)) {
+                      return 'Please Enter Amount';
+                    }
+                    return null;
+                  },
+                  // style: TextStyle(height: 0.5),
+                  decoration: CommonStyle.textFieldStyle(
+                      labelTextStr: "Amount", hintTextStr: "Enter Amount"),
+                ),
+                SizedBox(height: 20),
+                PaymentWidget(paymentInfo: _paymentInfo),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      ' Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      _dateTime == null
+                          ? 'Choose Date'
+                          : DateFormat.yMMMEd()
+                              .format(_dateTime ?? DateTime.now()),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(2020),
+                                  lastDate: DateTime(2025))
+                              .then((date) {
+                            setState(() {
+                              _dateTime = date;
+                            });
+                          });
+                        },
+                        child: Icon(Icons.calendar_today_outlined)),
+                  ],
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  keyboardType: TextInputType.name,
+                  controller: _approverNameController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please Enter Approver Name';
+                    } else if (!RegExp(
+                            r'^[a-zA-Z0-9]+(?:[\w -]*[a-zA-Z0-9]+)*$')
+                        .hasMatch(value)) {
+                      return 'Please Enter Correct Approver Name';
+                    }
+                    return null;
+                  },
+                  // style: TextStyle(height: 0.5),
+                  decoration: CommonStyle.textFieldStyle(
+                    labelTextStr: "Enter Approver Name",
+                    hintTextStr: "Enter Approver Name",
+                  ),
+                ),
+                SizedBox(height: 18),
+                ElevatedButton(
+                  style: CommonStyle.elevatedSubmitButtonStyle(),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Receipt receipt = Receipt(
+                        // accountCode: "SSMBSP",
+                        amount: double.parse(_amountController.text),
+                        devoteeId: "NA",
+
+                        paaliaName: _nameController.text,
+                        paymentMode: Utility.getPaymentModeString(
+                            _paymentInfo.paymentMode),
+                        paymentType: Utility.getPaymentTypeString(
+                            _paymentInfo.paymentType),
+                        preparedBy: Login.loggedInUser?.userId,
+                        receiptDate: DateTime.now(),
+                        receiptId: "",
+                        receiptNo: Utility.getReceiptNo(),
+                        transactionRefNo:
+                            _paymentInfo.paymentMode == Payment.bank
+                                ? _transactionController.text
+                                : null,
+                      );
+                      final receiptId = ReceiptAPI().createNewReceipt(receipt);
+                      print(receiptId);
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Data Submitted.')),
+                      );
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  ReceiptPreview(receipt: receipt)));
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
